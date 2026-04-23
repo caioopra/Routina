4224e0a5d613ab6e7d2a092003699707c4071097
caioopra <caioprsilva@gmail.com>
2026-04-23 13:51:27 -0300

    test(frontend): add localStorage shim for Zustand persist in jsdom env

--- frontend/src/test/setup.js
+++ frontend/src/test/setup.js
@@ -3,6 +3,37 @@ import { afterAll, afterEach, beforeAll } from "vitest";
 import { server } from "./mocks/server";
 import { resetMockState } from "./mocks/handlers";
 
+function createStorageShim() {
+  const store = new Map();
+  return {
+    getItem: (key) => (store.has(key) ? store.get(key) : null),
+    setItem: (key, value) => {
+      store.set(String(key), String(value));
+    },
+    removeItem: (key) => {
+      store.delete(key);
+    },
+    clear: () => {
+      store.clear();
+    },
+    key: (i) => Array.from(store.keys())[i] ?? null,
+    get length() {
+      return store.size;
+    },
+  };
+}
+
+Object.defineProperty(window, "localStorage", {
+  value: createStorageShim(),
+  writable: true,
+  configurable: true,
+});
+Object.defineProperty(window, "sessionStorage", {
+  value: createStorageShim(),
+  writable: true,
+  configurable: true,
+});
+
 Object.defineProperty(window, "matchMedia", {
   writable: true,
   value: (query) => ({
